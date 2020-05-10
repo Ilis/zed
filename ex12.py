c7198794e4ee6662e4eee4300cbf47af1cd8cc27
Ilis <ilis@inbox.ru>
2020-05-10 14:06:15 +0500

    ex12

---
+++ ex12.py
@@ -0,0 +1,7 @@
+age = input("Сколько тебе лет? ")
+height = input("Твой рост? ")
+weight = input("Твой вес? ")
+
+print(f"Итак, тебе {age} лет, "
+      f"в тебе {height} см роста, "
+      f"и ты весишь {weight} кг.")
