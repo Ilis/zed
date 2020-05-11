79f0e1f7a23af1141f39f4c816c626d9542c34a3
Ilis <ilis@inbox.ru>
2020-05-11 14:06:36 +0500

    Checkio test

--- helper.py
+++ helper.py
@@ -1,2 +1,7 @@
-for i in iter(input, ''):
-    print(i)
+def words_order(text: str, words: list) -> bool:
+    return [w for w in text.split() if w in words] == words
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(words_order('hi hi hi here', ['hi', 'here']))
