9b7ce2a2895a4f56e4b41165c57fae3cda651b4a
Ilis <ilis@inbox.ru>
2020-05-23 19:41:48 +0500

    ex15 fix unicode

--- ex15.py
+++ ex15.py
@@ -3,7 +3,7 @@ from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
+txt = open(filename, encoding="utf-8")
 
 print(f"Содержимое файла {filename}:")
 print(txt.read())
@@ -11,6 +11,6 @@ print(txt.read())
 print("Снова введите имя файла:")
 file_again = input("> ")
 
-txt_again = open(file_again)
+txt_again = open(file_again, encoding="utf-8")
 
 print(txt_again.read())
