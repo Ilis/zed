475abe340268e2c993cd6b860f94b7f8222fc636
Ilis <ilis@inbox.ru>
2020-06-07 12:23:46 +0500

    ex18

---
+++ ex18.py
@@ -0,0 +1,22 @@
+# Like a scripts
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+
+def print_none():
+    print("Nothing here")
+
+
+print_two("Micael", "Rightman")
+print_two_again("Michal", "Fightman")
+print_one("The 1st")
+print_none()
