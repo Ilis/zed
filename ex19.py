3932d988f5dd10e5a002f2d84df2d3d1e12d711e
Илья <ilis@IT22.ekb.corp.motiv>
2020-06-20 14:38:55 +0500

    ex19

---
+++ ex19.py
@@ -0,0 +1,21 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    """Печатает количество сырков и чипсов, которых достаточно!"""
+    print(f"У нас есть {cheese_count} сырков!")
+    print(f"У нас есть {boxes_of_crackers} пачек чипсов!")
+    print("Этого достаточно для вечеринки!\nПогнали!\n")
+
+
+print("Мы можем передать числа")
+cheese_and_crackers(20, 30)
+
+print("Или мы можем передать переменные")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("Мы можем выполнять вычисления")
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print("И объединять переменные с числами")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
