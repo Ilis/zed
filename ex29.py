da89d67949504e9ddc8278b64140a424daaa51b0
Ilis <ilis@inbox.ru>
2020-07-25 22:24:04 +0500

    ex29

---
+++ ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats")
+
+if people > cats:
+    print("Not too many cats")
+
+if people < dogs:
+    print("Dogs win")
+
+if people > dogs:
+    print("Dogs lose")
+
+dogs += 5
+
+if people >= dogs:
+    print("People more or equal then dogs")
+
+if people <= dogs:
+    print("People less or equal then dogs")
+
+if people == dogs:
+    print("People are equal to dogs")
